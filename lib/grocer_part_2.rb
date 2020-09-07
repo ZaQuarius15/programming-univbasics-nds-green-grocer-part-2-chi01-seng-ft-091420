9803798937f9950c7e6e68701bf7cf5b094b8d61
Zack Carlson <jzcarlson014@gmail.com>
2020-09-07 22:06:33 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,10 +5,27 @@ require 'pry'
 def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length do
-    item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    groc_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    coup_name = "#{coupons[counter][:item]} W/COUPON"
+    coup_item = find_item_by_name_in_collection(coup_name, cart)
+    if groc_item && groc_item[:count] >= coupons[counter][:num]
+      if coup_item
+        coup_item[:count] += coupons[counter][:num]
+        groc_item[:count] -= coupons[counter][:num]
+      else 
+        coup_item = {
+          :item => coup_name,
+          :price => coupons[counter][:cost] / coupons[counter][:num],
+          :clearance => groc_item[:clearance],
+          :count => coupons[counter][:num]
+        }
+        cart << coup_item
+        groc_item[:count] -= coupons[counter][:num]
+      end
+    end
     counter += 1
   end
-  
+  cart
 end
 
 def apply_clearance(cart)
